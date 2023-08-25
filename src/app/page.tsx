c96e5a251cb17d2d65a3e50cee704b13af551683
do-ald533 <douglas.aldred533@gmail.com>
2023-08-25 19:41:17 -0300

    style(eslint): styling file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import { TodoList } from "@/app/_components";
+import { TodoList } from "@/app/_components/";
 
 export default function Home() {
 	return (
 		<>
-			<h1>Hello world</h1> 
-      <TodoList />
+			<h1>Hello world</h1>
+			<TodoList />
 		</>
 	);
 }
